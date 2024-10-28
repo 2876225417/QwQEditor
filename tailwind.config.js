12ca22f604351f35b20a2d96bf1b2454ac9ff90c
ppQwQqq <61179629+2876225417@users.noreply.github.com>
2024-10-28 13:07:48 +0800

    qwqeditor v1.0.4 Ajusted Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./qwqeditor/**/*.html",
+      "./qwqeditor/**/*.js",
+      "./qwqeditor/**/*.jsx",
+      "./qwqeditor/**/*.ts",
+      "./qwqeditor/**/*.tsx",
+      "./qwqeditor/**/*.ejs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
